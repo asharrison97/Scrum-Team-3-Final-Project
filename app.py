6d04cfedad1b10849140bff656703f60e87b835b
WesGale05 <wesjgale05@gmail.com>
2025-05-05 20:51:34 -0500

    more functional database(s) - added admin db

--- app.py
+++ app.py
@@ -28,40 +28,52 @@ def reservations():
     return render_template('reservations.html')
 
 #get a db connection
-conn = sqlite3.connect('reservations.db')
-conn.row_factory = dict_factory
-cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS reservations
-(first text, last text, row test, col real, code integer unique)''')
-# Insert a row of data
+connR = sqlite3.connect('reservations.db') # reservations connection
+connA = sqlite3.connect('admin.db') #admin connection
+connR.row_factory = dict_factory
+connA.row_factory = dict_factory
+cursorR = connR.cursor() #reservations cursor
+cursorA = connA.cursor() #admin cursor
+
+#initiate tables if not already 
+cursorR.execute('''CREATE TABLE IF NOT EXISTS reservations
+(first text, last text, row test, col real, code integer)''')
+cursorA.execute('''CREATE TABLE IF NOT EXISTS admin
+(user text, password text)''') 
+
+# Insert a row of data testing
 
 #trying to get it to show in the db viewer
-try:
-    cursor.execute("INSERT INTO reservations VALUES ('Amanda', 'Smith', 'E', 54, 627485)")
-    conn.commit()
-except sqlite3.Error as e:
-    print(f"An error occurred: {e}")
+# try:
+#     cursor.execute("INSERT INTO reservations VALUES ('Amanda', 'Smith', 'E', 54, 627485)")
+#     conn.commit()
+# except sqlite3.Error as e:
+#     print(f"An error occurred: {e}")
 #cursor.execute("INSERT INTO reservations VALUES ('Gerry','Neil','A',61,364758)")
 
+#cursorA.execute("INSERT INTO admin VALUES ('AdminBigBoss123','$pot1987')")
+
 # Save (commit) the changes
-#conn.commit()
+connA.commit()
 
 
 # Query the data
-cursor.execute("SELECT * FROM reservations")
-#selects all
 
 # Fetch all rows
-rows = cursor.fetchall()
+rowsR = cursorR.execute("SELECT * FROM reservations").fetchall()
+rowsA = cursorA.execute("SELECT * FROM admin").fetchall()
 # Close the connection
-conn.close()
+connR.close()
+connA.close()
 
 #printing database rows for testing
-for row in rows:
+for row in rowsR:
     print(row) #prints correctly
 
+#printing database rows for testing
+for row in rowsA:
+    print(row) #prints correctly
 
-#all_reservations = cursor.execute().fetchall()
 
 #run the application
 app.run()
